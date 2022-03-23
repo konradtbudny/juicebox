f99c99b02b71cca28424931c0f481176a2ebdc11
konradtbudny <konradtbudny@gmail.com>
2022-03-23 12:30:06 -0500

    expanding createUser

--- db/index.js
+++ db/index.js
@@ -1,32 +1,36 @@
-const { Client } = require('pg'); // imports the pg module
-const { rows } = require('pg/lib/defaults');
+const { Client } = require("pg"); // imports the pg module
+const { rows } = require("pg/lib/defaults");
 
-const client = new Client('postgres://localhost:5432/juicebox-dev');
+const client = new Client("postgres://localhost:5432/juicebox-dev");
 
-async function getAllUsers(){
-    const {rows} = await client.query(
-        `SELECT id, username
+async function getAllUsers() {
+  const { rows } = await client.query(
+    `SELECT id, username, name, location, active
         FROM users;
-        `);
-    return rows;
+        `
+  );
+  return rows;
 }
-async function createUser({ username, password }) {
-    try {
-      const result = await client.query(`
-      INSERT INTO users(username, password)
-      VALUES ($1,$2)
+async function createUser({ username, password, name, location }) {
+  try {
+    const result = await client.query(
+      `
+      INSERT INTO users(username, password, name, location)
+      VALUES ($1,$2,$3,$4)
       ON CONFLICT (username) DO NOTHING
       RETURNING *;
-      `,[username,password]);
-  
-      return rows
-    } catch (error) {
-      throw error;
-    }
+      `,
+      [username, password, name, location]
+    );
+
+    return rows;
+  } catch (error) {
+    throw error;
   }
+}
 
 module.exports = {
   client,
   getAllUsers,
   createUser,
-}
+};
--- db/seed.js
+++ db/seed.js
@@ -4,10 +4,9 @@ async function createInitialUsers() {
     try {
       console.log("Starting to create users...");
   
-      const albert = await createUser({ username: 'albert', password: 'bertie99' });
-      const sandra = await createUser({ username: 'sandra', password: '2sandy4me' });
-      const glamgal = await createUser({ username: 'glamgal', password: 'soglam' });
-      console.log(albert);
+      const albert = await createUser({ username: 'albert', password: 'bertie99', name: 'Al Bert', location: 'Sidney, Australia'});
+      const sandra = await createUser({ username: 'sandra', password: '2sandy4me', name: 'Just Sandra', location: "Ain't tellin"});
+      const glamgal = await createUser({ username: 'glamgal', password: 'soglam', name: 'Joshua', location: 'Upper East Side'});
   
       console.log("Finished creating users!");
     } catch(error) {
@@ -33,7 +32,11 @@ async function createTables() {
     await client.query(`CREATE TABLE users(
             id SERIAL PRIMARY KEY,
             username varchar(255) UNIQUE NOT NULL,
-            password varchar(255) NOT NULL);
+            password varchar(255) NOT NULL,
+            name VARCHAR(255) NOT NULL,
+            location VARCHAR(255) NOT NULL,
+            active BOOLEAN DEFAULT true
+            );
       `);
     console.log("Finished building tables!");
   } catch (error) {
